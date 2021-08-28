070f5a81bfce98e624513e87dac5572248c1b7c6
nottimnik <timnikofficial@gmail.com>
2021-08-28 17:28:03 +0300

    Added Animation

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -6,6 +6,9 @@ const FRICTION = 500 #The FRICTION that the player can have
 
 var vector = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+onready var sprite = $Sprite
+
 #Basic Player Movement
 
 func _physics_process(delta):
@@ -19,11 +22,23 @@ func _physics_process(delta):
 	
 	#checks if the user pressed any key by checking if the input_vector is changed or not
 	if input_vector != Vector2.ZERO:
+		
+		#if the input will be positive that means that the player is going to the right direction
+		if input_vector.x > 0:
+			sprite.flip_h = false #flips the sprite to the right
+			animationPlayer.play("Run")
+			
+		#else the player is going to the left direction
+		else:
+			sprite.flip_h = true #flipts the sprite to the left
+			animationPlayer.play("Run")
+		
 		vector = vector.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	
 	#if the user didn't pressed any key then we don't update the vector
 	#and make the player stop
 	else:
+		animationPlayer.play("Idle")
 		vector = vector.move_toward(Vector2.ZERO, FRICTION * delta) #apply friction to the player when he stops
 		
 	vector = move_and_slide(vector) #move the player
